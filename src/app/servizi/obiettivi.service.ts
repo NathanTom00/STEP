1ab982d568022e2ec5e5bff73cba2ad9cc082bb9
Nathaniele Tomines <nathan.tomines00@gmail.com>
2024-01-02 22:19:15 +0100

    aggiunto servizio obiettivi

---
+++ src/app/servizi/obiettivi.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObiettiviService {
+  private iconLink = "../../../assets/Icons/"
+  obiettivi = [
+    {nome: 'scoprire cose nuove',icon: `${this.iconLink}scoprire-cose-nuove-icon.svg`},
+    {nome: "approfondire temi d'interesse", icon: `${this.iconLink}temi-interesse.svg`},
+    {nome: 'solitudine', icon: `${this.iconLink}solitudine.svg`},
+    {nome: 'pace', icon: `${this.iconLink}pace.svg`},
+    {nome: 'ispirazione', icon: `${this.iconLink}ispirazione.svg`},
+    {nome: 'avventura', icon: `${this.iconLink}avventura.svg`},
+    {nome: 'evasione', icon: `${this.iconLink}evasione.svg`},
+    {nome: 'libertà', icon: `${this.iconLink}libertà.svg`},
+    {nome: 'curiosità', icon: `${this.iconLink}curiosità.svg`},
+    {nome: 'relax', icon: `${this.iconLink}relax.svg`},
+    {nome: 'divertimento', icon: `${this.iconLink}divertimento.svg`},
+    {nome: 'migliorare le mie relazioni', icon: `${this.iconLink}relazioni.svg`},
+    {nome: 'tempo per me stesso', icon: `${this.iconLink}tempo.svg`},
+    {nome: 'energia', icon: `${this.iconLink}energia.svg`},
+    {nome: 'riconnettermi con me stesso', icon: `${this.iconLink}riconnettermi.svg`}
+  ]
+  constructor() { }
+}
--- src/app/servizi/onboarding.service.ts
+++ src/app/servizi/onboarding.service.ts
@@ -1,34 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable, filter, map, tap } from 'rxjs';
+import { ObiettiviService } from './obiettivi.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnboardingService {
-  iconLink = "../../../assets/Icons/"
-  obiettivi = [
-    {nome: 'scoprire cose nuove',icon: `${this.iconLink}scoprire-cose-nuove-icon.svg`},
-    {nome: "approfondire temi d'interesse", icon: `${this.iconLink}temi-interesse.svg`},
-    {nome: 'solitudine', icon: `${this.iconLink}solitudine.svg`},
-    {nome: 'pace', icon: `${this.iconLink}pace.svg`},
-    {nome: 'ispirazione', icon: `${this.iconLink}ispirazione.svg`},
-    {nome: 'avventura', icon: `${this.iconLink}avventura.svg`},
-    {nome: 'evasione', icon: `${this.iconLink}evasione.svg`},
-    {nome: 'libertà', icon: `${this.iconLink}libertà.svg`},
-    {nome: 'curiosità', icon: `${this.iconLink}curiosità.svg`},
-    {nome: 'relax', icon: `${this.iconLink}relax.svg`},
-    {nome: 'divertimento', icon: `${this.iconLink}divertimento.svg`},
-    {nome: 'migliorare le mie relazioni', icon: `${this.iconLink}relazioni.svg`},
-    {nome: 'tempo per me stesso', icon: `${this.iconLink}tempo.svg`},
-    {nome: 'energia', icon: `${this.iconLink}energia.svg`},
-    {nome: 'riconnettermi con me stesso', icon: `${this.iconLink}riconnettermi.svg`}
-  ]
-
-
+  obiettivi : {}[];
   obiettiviSelezionati : string[]= []
 
-  constructor(private firestore: Firestore) {}
+  constructor(private firestore: Firestore, private obiettiviService : ObiettiviService) {
+    this.obiettivi = obiettiviService.obiettivi
+  }
 
   getObiettivi(){
     return this.obiettivi
@@ -42,23 +26,12 @@ export class OnboardingService {
     return this.obiettiviSelezionati
   }
 
-  cercaLuoghiPerObiettivi(){
-    //prendo la lista di tutti i luoghi sul firestore
-    const collezioneLuoghi = collection(this.firestore, 'luogo');
-    /**
-    collectionData(collezioneLuoghi).subscribe((luoghi:any) => {
-      console.log(this.obiettiviSelezionati)
-      for(let luogo of luoghi){
-        if(this.obiettiviSelezionati.some(obiettivo => luogo.obiettivi.includes(obiettivo))){
-          
-          console.log(luogo)
-        }
-      }
-    })
-     */
 
+  cercaLuoghiPerObiettivi(){
+    const collezioneLuoghi = collection(this.firestore, 'luogo');
     return collectionData(collezioneLuoghi, { idField: 'id'}).pipe(
       map((luoghi:any[]) => {
+        //ritorna i luoghi del db che hanno incluso uno dei 3 obiettivi selezionati
         return luoghi.filter((luogo:any) => this.obiettiviSelezionati.some(obiettivo => luogo.obiettivi.includes(obiettivo)))
       })
     )
