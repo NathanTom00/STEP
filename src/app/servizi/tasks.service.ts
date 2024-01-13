d4b25597d3df2258210a0c6f67288ca0bbc39edb
Nathaniele Tomines <nathan.tomines00@gmail.com>
2024-01-13 02:10:36 +0100

    fix testi

--- src/app/servizi/tasks.service.ts
+++ src/app/servizi/tasks.service.ts
@@ -8,7 +8,7 @@ export class TasksService {
   tasks = [
     {nome: 'Traveller', descrizione: 'Esplora 3 luoghi'},
     {nome: "Because I'm happy", descrizione: 'Cerca gioia'},
-    {nome: 'Soul search', descrizione: 'Esplora un anima locus'},
+    {nome: 'Soul search', descrizione: "Esplora un'anima locus"},
     {nome: 'Getting emotional', descrizione: "Aggiungi un'emozione a un luogo"},
     {nome: 'Traveller level 2', descrizione: 'Esplora 5 luoghi'},
   ]
